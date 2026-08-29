e9b8f9787c15326a0b3d67b75e0cf7e09276a3d2
Milan Szabo <stealman@seznam.cz>
2026-08-29 23:05:04 +0200

    DEV

--- src/babylon/item/codebook/weaponModelsCb.ts
+++ src/babylon/item/codebook/weaponModelsCb.ts
@@ -9,7 +9,7 @@ import { EquipSlotModelsCb } from '@/data/items/item'
 
 const matBowSize = new Vector2(4, 1)
 const matLongswordSize = new Vector2(4, 1)
-const matBroadswordSize = new Vector2(4, 1)
+const matBroadswordSize = new Vector2(1, 1)
 const matPickaxeSize = new Vector2(4, 1)
 const matGreatAxeSize = new Vector2(4, 1)
 const matFutureWeaponSize = new Vector2(5, 1)
@@ -30,7 +30,7 @@ export const WeaponsCbManager = {
         // Load materials
         this.bowMaterial = this.getMaterial("hunterbow", matBowSize)
         this.longSwordMaterial = this.getMaterial("longsword", matLongswordSize)
-        this.broadSwordMaterial = this.getMaterial("broadsword", matBroadswordSize)
+        this.broadSwordMaterial = this.getMaterial("broadsword2", matBroadswordSize)
         this.pickAxeMaterial = this.getMaterial("pickaxe", matPickaxeSize)
         this.greatAxeMaterial = this.getMaterial("greataxe", matGreatAxeSize)
 
@@ -76,7 +76,7 @@ export const WeaponsCbManager = {
 export const WeaponModelsCb = {
     LONGSWORD: new EquipCbItem(EquipSlotModelsCb.LONGSWORD.modelId, "longsword", Vector3.Zero(), new Vector3(0.2, 0.24, 0.4), new Vector3(0, 2.4, 0), matLongswordSize),
 
-    BROADSWORD: new EquipCbItem(EquipSlotModelsCb.BROADSWORD.modelId, "broadsword", Vector3.Zero(), new Vector3(0.22, 0.24, 0.4), new Vector3(0, 2, 0), matBroadswordSize),
+    BROADSWORD: new EquipCbItem(EquipSlotModelsCb.BROADSWORD.modelId, "broadsword2", Vector3.Zero(), new Vector3(0.22, 0.24, 0.4), new Vector3(0, 2, 0), matBroadswordSize),
 
     GREATSWORD: new EquipCbItem(EquipSlotModelsCb.GREATSWORD.modelId, "greatsword", Vector3.Zero(), new Vector3(0.22, 0.24, 0.4), new Vector3(0, 2.5, 0), matFutureWeaponSize),
 
